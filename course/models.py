9f246d3aa9aec40928de8fe8ba9a791f3a02526d
hossein <hossein.mansouri11@gmail.com>
2021-11-14 01:42:02 +0330

    remove slug filed in Category model.

--- course/models.py
+++ course/models.py
@@ -12,14 +12,9 @@ class Hashtag(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=200, unique=True)
 
     def __str__(self):
         return self.title
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super(Category, self).save(*args, **kwargs)
 
 
 class Course(models.Model):
