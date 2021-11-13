33c9138659831db7b2d8ecf1ce3184afa83c92b1
hossein <hossein.mansouri11@gmail.com>
2021-11-14 02:13:36 +0330

    add image filed to Category Model.

--- course/models.py
+++ course/models.py
@@ -7,9 +7,13 @@ from user.models import User
 class Hashtag(models.Model):
     title = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.title
+
 
 class Category(models.Model):
     title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='categories_images/')
 
     def __str__(self):
         return self.title
