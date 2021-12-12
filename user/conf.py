66b52909470baa075386c27137bdf83f06eae09d
hossein <hossein.mansouri11@gmail.com>
2021-12-12 06:13:41 +0330

    move some messages to conf.py to preventing hard coding.

--- user/conf.py
+++ user/conf.py
@@ -3,3 +3,7 @@ from django.utils.translation import gettext_lazy as _
 USER_CREATION_MESSAGE = _('User created successfully')
 
 PASSWORD_CHANGED_MESSAGE = _('Password changed successfully')
+
+PASSWORDS_MATCH_MESSAGE = _("Password fields didn't match.")
+
+OLD_PASSWORD_MESSAGE = _("Old password is not correct")
--- user/utils.py
+++ user/utils.py
@@ -1,15 +1,15 @@
 from django.contrib.auth import password_validation
 from django.http import Http404
-from django.utils.translation import gettext_lazy as _
 
 from rest_framework import serializers
 
+from user.conf import PASSWORDS_MATCH_MESSAGE, OLD_PASSWORD_MESSAGE
 from user.models import User
 
 
 def check_new_password(password1, password2, user=None):
     if (password1 and password2) and (password1 != password2):
-        raise serializers.ValidationError(_("Password fields didn't match."))
+        raise serializers.ValidationError({"detail": PASSWORDS_MATCH_MESSAGE})
 
     password_validation.validate_password(password2, user)
     return password2
@@ -18,7 +18,7 @@ def check_new_password(password1, password2, user=None):
 def check_old_password(user, old_password):
     if user.check_password(old_password):
         return old_password
-    raise serializers.ValidationError({"detail": "Old password is not correct"})
+    raise serializers.ValidationError({"detail": OLD_PASSWORD_MESSAGE})
 
 
 def get_user_or_404(user_id):
